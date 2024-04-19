67918bd08e5381edb330828808ea053f879e5d36
Sarah Kota <sarahkota@Sarahs-MacBook-Air.local>
2024-04-19 15:36:09 -0400

    updated image processing

--- imageProcessing.py
+++ imageProcessing.py
@@ -3,18 +3,26 @@ import cv2
 import numpy as np
 
 # Import the image
-image = Image.open('Circle.jpg')
+image = Image.open('Strongyle2.png')
 
 # Test if the image loaded correctly
-image = cv2.imread('Circle.jpg')
+image = cv2.imread('Strongyle2.png')
 if image is None:
     print("Error: Unable to load image.")
-    
-# Read the image in grayscale
-image = cv2.imread('Circle.jpg', cv2.IMREAD_GRAYSCALE)
 
-# Apply a Gaussian blur to reduce noise
-blurredImage = cv2.GaussianBlur(image, (3, 3), 0)
+# Read the image in grayscale
+image = cv2.imread('Strongyle2.png', cv2.IMREAD_GRAYSCALE)
+
+# # Apply a Gaussian blur to reduce noise
+# blurredImage = cv2.GaussianBlur(image, (5, 5), 0)
+
+blurredImage = image.copy()
+for _ in range(3):  # Apply 5 iterations (you can adjust the number as needed)
+    blurredImage = cv2.GaussianBlur(blurredImage, (3, 3), 0)
+
+
+# Apply Canny edge detection to detect edges
+edges = cv2.Canny(blurredImage, 100, 200)
 
 # Apply the Laplacian filter
 laplacianImage = cv2.Laplacian(blurredImage, cv2.CV_64F)
@@ -22,8 +30,14 @@ laplacianImage = cv2.Laplacian(blurredImage, cv2.CV_64F)
 # Convert the result back to an 8-bit image
 sharpImage = np.uint8(np.clip(laplacianImage, 0, 255))
 
-# Display the original and sharpened images
+# Apply thresholding to highlight edges
+
+_, edgeImage = cv2.threshold(sharpImage, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# Display the original, sharpened, and edges images
 cv2.imshow('Original', image)
 cv2.imshow('Sharpened', sharpImage)
+cv2.imshow("Edges", edgeImage)
+cv2.imshow("secondEdges", edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
